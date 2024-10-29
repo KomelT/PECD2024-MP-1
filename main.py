05d8d05951609d32cce1a3e6b774d9aa5f1ff0d2
Telmo Sendino <telmotxo@gmail.com>
2024-10-29 15:16:29 +0100

    changes

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from parser import *
 from sensors import *
 from time import sleep
-import board
+from board import D14
 
 # Read configuration file
 c = Parser()
 c.read_conf()
-c.print_range_values()
-
 
 # Config sensors
-s = Sensors(board.D14)
+s = Sensors(D14)
 
-while True:
-  print(s.read_temp_humid())
-  sleep(1)
+
+s.read_soil_humid()
