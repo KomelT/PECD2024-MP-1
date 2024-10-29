34ab42bde92f4fd5bb715d1d5583a80e57f6ad98
Telmo Sendino <telmotxo@gmail.com>
2024-10-29 15:07:45 +0100

    function to read soil humidity added

---
+++ logs/__init__.py
@@ -0,0 +1,15 @@
+import datetime
+def log_event(message):
+    fd = open("./events_log.txt","a")
+    fd.write(f"{datetime.datetime.now().astimezone().strftime('%a %b %d %H:%M:%S %Z %Y')}: {message}\n")
+    
+    
+        
+ 
+
+
+ 
+ 
+        
+        
+    
--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,11 +1,22 @@
 from adafruit_dht import DHT11
 from logs import log_event
+from time import sleep
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
 
 class Sensors:
-  def __init__(self, temp_humid_pin):
+  def __init__(self, temp_humid_pin, soil_humid_cs_pin=board.D5):
     self.temp_humid_pin = temp_humid_pin
-    
     self.temp_humid = DHT11(temp_humid_pin)
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    cs = digitalio.DigitalInOut(soil_humid_cs_pin)
+    self.mcp = MCP.MCP3008(spi, cs)
+    
     
   def read_temp_humid(self):
     for i in range(10):
@@ -15,3 +26,15 @@ class Sensors:
       except RuntimeError as e:
         log_event(f"[ERROR] While reading temp and humidity: {e}")
         continue
+      
+
+      
+  def read_soil_humid(self):
+    channel = AnalogIn(mcp, MCP.P0)
+    while True:
+	    print('Raw ADC Value: ', channel.value)
+	    print('ADC Voltage: ' + str(channel.voltage) + 'V')
+	    sleep(1)
+
+
+    
