0583bf8cc13daf07ea25062ee6c4286f480a9858
me@komelt.dev <me@komelt.dev>
2024-10-30 15:53:48 +0100

    Refactor main.py to initialize buzzer and sensors, and add temperature and humidity reading

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from parser import *
+from buzzer import *
 from sensors import *
-from time import sleep
-from board import D14
 
-# Read configuration file
-c = Parser()
-c.read_conf()
+# GPIO mode is set to BCM, because of adafruit_dht library
+# What? https://raspi.tv/2013/rpi-gpio-basics-4-setting-up-rpi-gpio-numbering-systems-and-inputs
 
-# Config sensors
-s = Sensors(D14)
+# Init buzzer
+b = Buzzer()
+b.buzz_x_times(2)
 
+# Init sensors
+s = Sensors()
+print(s.read_temp_humid())
 
-s.read_soil_humid()
+# Clean GPIO before exit
+GPIO.cleanup()
