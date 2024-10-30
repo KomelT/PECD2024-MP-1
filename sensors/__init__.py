e6f7a29adaf42c066184482c9cbf0028bf0624e5
Tilen Komel <me@komelt.dev>
2024-10-30 19:12:24 +0100

    Add log air temperature and humidity readings

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -23,6 +23,8 @@ class Sensors:
         for i in range(10):
             try:
                 res = self.temp_humid
+                log_event(f"air_temperature = {res.temperature}")
+                log_event(f"air_humidity = {res.humidity}")
                 return (res.temperature, res.humidity)
             except RuntimeError as e:
                 log_event(f"[ERROR] While reading temp and humidity: {e}")
