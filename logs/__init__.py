28c8312621582fbbdb581eec221c314b9fd342ae
me@komelt.dev <me@komelt.dev>
2024-10-30 15:53:36 +0100

    Refactor logging function to adhere to file naming conventions

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,15 +1,8 @@
 import datetime
+
+
 def log_event(message):
-    fd = open("./events_log.txt","a")
-    fd.write(f"{datetime.datetime.now().astimezone().strftime('%a %b %d %H:%M:%S %Z %Y')}: {message}\n")
-    
-    
-        
- 
-
-
- 
- 
-        
-        
-    
+    fd = open("./events_log.txt", "a")
+    fd.write(
+        f"{datetime.datetime.now().astimezone().strftime('%a %b %d %H:%M:%S %Z %Y')}: {message}\n"
+    )
