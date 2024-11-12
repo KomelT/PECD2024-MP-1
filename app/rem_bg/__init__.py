d2a3378ef0750d5540768bac02180e4e5f0d5f69
Telmo <sendINUX@users.noreply.github.com>
2024-11-12 15:09:25 +0100

    now the server returns the mean of green,yellow and black colors

--- app/rem_bg/__init__.py
+++ app/rem_bg/__init__.py
@@ -1,11 +1,11 @@
 import socket
 import numpy as np
 from io import BytesIO
+import cv2
 
 '''
 THIS IS THE CODE FOR THE CLIENT-SIDE of the background removal API
 '''
-
 PORT = 8000
 
 
@@ -14,46 +14,43 @@ def get_size(buf):
     if header=='SIZE':
         size = int.from_bytes(buf[5:9],byteorder='little') 
         return size
-    print('something was wrong while decoding the size from the header')
-    raise RuntimeError
+    raise RuntimeError("Decodification of the size parameter from the header failed. (Wrong header?)")
 
 # pic must be bytes
-def remove_bg_send(pic, dir_serv,size):
+def get_leaf_color_means(pic, dir_serv,size):
     s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
     s.connect((dir_serv,PORT) )
-    dt = np.uint8
 
-    while True:
-        if not pic:
-            print("Image is empty")
-            break
-        size_b =  int.to_bytes(size,4,byteorder='little')  
+    if not pic:
+        raise ValueError("Image is empty")
+    size_b =  int.to_bytes(size,4,byteorder='little')  
+    
+    s.sendall(b'SIZE ' + size_b + b' '+ pic)
+    print(f"Image sended ( {size} bytes )")
+
+    cont = 0
+    # first block and size
+    buf = s.recv(256)
+    cont += len(buf)
+    size = get_size(buf)
         
-        s.sendall(b'SIZE ' + size_b + b' '+ pic)
-        print(f"Image sended ( {size} bytes )")
-
-        cont = 0
-        # first block and size
-        buf = s.recv(256)
+    calculated_means = buf[10:]
+    while cont < size:
+        buf = s.recv(4096)
+        calculated_means += buf
         cont += len(buf)
-        size = get_size(buf)
-           
-        processed_pic = buf[10:]
-        while cont < size:
-            buf = s.recv(4096)
-            processed_pic += buf
-            cont += len(buf)
-        return (processed_pic)
+    mean_arr = np.frombuffer(calculated_means, dtype=float) # cv2.IMREAD_COLOR in OpenCV 3.1
     s.close()
+    return (mean_arr)
  
     
 if __name__ == "__main__":
-    with open("../foto.jpg", "rb") as fd:
+    with open("../plant.jpg", "rb") as fd:
         pic = BytesIO(fd.read()).getvalue()
     
     size = len(pic)
-    output_bytes = remove_bg_send(pic,"127.0.0.1",size)
-    
-    with open("output_main.png", "wb") as f:
-        f.write(output_bytes)
+    mean_arr = get_leaf_color_means(pic,"127.0.0.1",size)
+    print(f"Green mean: {mean_arr[0]} \n Yellow mean: {mean_arr[1]} \n Black mean: {mean_arr[2]}")
+
+
     
