50bcc1c1e7b298a11da995e5ea350b6d237c4f89
Tilen Komel <me@komelt.dev>
2024-11-13 12:20:01 +0100

    Simplified camera code

--- app/camera/__init__.py
+++ app/camera/__init__.py
@@ -1,75 +1,28 @@
-
-import cv2
-from PIL import Image
+from picamera2 import Picamera2
 import time
-import numpy as np
-from time import sleep
-import matplotlib.pyplot as plt
 from io import BytesIO
-
-from picamera2 import Picamera2 
-
-
-import socket
-import numpy as np
-#from io import BytesIO
-#import cv2
-
-'''
-THIS IS THE CODE FOR THE CLIENT-SIDE of the background removal API
-'''
-PORT = 8000
+import requests
 
 
-def get_size(buf):
-    header = buf[:4].decode()
-    if header=='SIZE':
-        size = int.from_bytes(buf[5:9],byteorder='little') 
-        return size
-    raise RuntimeError("Decodification of the size parameter from the header failed. (Wrong header?)")
+class Camera:
+    def __init__(self, sever_address="http://localhost:5000"):
+        self.sever_address = sever_address
 
-# pic must be bytes
-def get_leaf_color_means(pic, dir_serv,size):
-    s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    s.connect((dir_serv,PORT) )
+    def get_percentage(self):
 
-    if not pic:
-        raise ValueError("Image is empty")
-    size_b =  int.to_bytes(size,4,byteorder='little')  
-    
-    s.sendall(b'SIZE ' + size_b + b' '+ pic)
-    print(f"Image sended ( {size} bytes )")
+        # Init the camera
+        picam2 = Picamera2()
+        config = picam2.create_still_configuration(
+            {"format": "RGB888", "size": (4056, 3040)}
+        )
+        picam2.configure(config)
+        picam2.start()
 
-    cont = 0
-    # first block and size
-    buf = s.recv(256)
-    cont += len(buf)
-    size = get_size(buf)
-        
-    calculated_means = buf[10:]
-    while cont < size:
-        buf = s.recv(4096)
-        calculated_means += buf
-        cont += len(buf)
-    mean_arr = np.frombuffer(calculated_means, dtype=float) # cv2.IMREAD_COLOR in OpenCV 3.1
-    s.close()
-    return (mean_arr)
- 
+        time.sleep(2)
 
+        pic = BytesIO()
+        picam2.capture_file(pic, format="jpeg")
+        picam2.stop()
 
-def get_percentage():
-    picam2 = Picamera2()
-    config = picam2.create_still_configuration({'format': 'RGB888', 'size': (4056, 3040)})
-    picam2.configure(config)
-
-    picam2.start()
-
-    time.sleep(2)
-    data = BytesIO()
-    picam2.capture_file(data, format='jpeg')
-    picam2.stop()
-    data_b = data.getvalue()
-    size = len(data_b)
-    mean_arr = get_leaf_color_means(data_b,"ordenador",size)
-    print(f"Green mean: {mean_arr[0]} \n Yellow mean: {mean_arr[1]} \n Black mean: {mean_arr[2]}")
-
+        r = requests.post(self.sever_address, files={"image": pic.getvalue()})
+        return r.json()["yellow_percentage"]
--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
-from buzzer import *
-from sensors import *
-
-from camera import get_percentage
+from buzzer import Buzzer
+from sensors import Sensors
+from camera import Camera
 
 # GPIO mode is set to BCM, because of adafruit_dht library
 # What? https://raspi.tv/2013/rpi-gpio-basics-4-setting-up-rpi-gpio-numbering-systems-and-inputs
 
 # Init buzzer
-#b = Buzzer()
-#b.buzz_x_times(1)
+b = Buzzer()
+b.buzz_x_times(1)
 
 # Init sensors
-#s = Sensors()
-#print(f"DHT11: {s.read_temp_humid()}")
-#print(f"Soil sens: {s.read_soil_humid()}")
+s = Sensors()
+print(f"DHT11: {s.read_temp_humid()}")
+print(f"Soil sensor: {s.read_soil_humid()}")
+
+# Init camera
+c = Camera()
+print(f"Yellow percentage: {c.get_percentage()}")
 
 # Clean GPIO before exit
 GPIO.cleanup()
-
-get_percentage()
